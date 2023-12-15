7a8967f4cd83af1766e60eee55f70e066c14d35f
aaronwai <aaron.lung.wai@gmail.com>
2023-12-15 23:37:06 +0800

    Step 20 edit record from the input form

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -65,7 +65,6 @@ class ListingController {
         $newListingData = array_map('sanitize', $newListingData);
         // prepare required fields
         $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
-
         $errors = [];
         //  check empty and not string for required fields, then put error message 
         foreach ($requiredFields as $field) {
@@ -73,7 +72,6 @@ class ListingController {
                 $errors[$field] = ucfirst($field) . ' is required';
             }
         }
-
         if (!empty($errors)) {
             // Reload view with errors, and keep the input passing back to the form
             loadView('listings/create', [
@@ -87,7 +85,6 @@ class ListingController {
             $fields[] = $field;
         }
         $fields = implode(', ', $fields); 
-
         $values = [];
         // add field names into fields array
         foreach($newListingData as $field => $value) {
@@ -100,7 +97,6 @@ class ListingController {
         $values = implode(', ', $values); 
         $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
         $this->db->query($query, $newListingData);
-
         // add redirect to a given url 
         redirect('/listings');
         }
@@ -126,5 +122,77 @@ class ListingController {
         //  add flash message
         $_SESSION['success_message'] = 'Listing successfully deleted !';
         redirect('/listings');
+    }
+
+    /**
+     * show the listing in the edit form
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function edit($params) {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+        if (!$listing) {
+            ErrorController::notFound('Listing not found !');
+            return;
+        }
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * Update a listing
+     * 
+     * @param array $param
+     * @return void
+     */
+    public function update($params) {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+        if (!$listing) {
+            ErrorController::notFound('Listing not found !');
+            return;
+        } 
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+        $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+        $updateValues = array_map('sanitize', $updateValues);
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
+        $errors = [];
+        //  check empty and not string for required fields, then put error message 
+        foreach ($requiredFields as $field) {
+            if (empty($updateValues[$field]) || !Validation::string($updateValues[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+        if (!empty($errors)) {
+            // Reload view with errors, and keep the input passing back to the form
+            loadView('listings/edit', [
+            'errors' => $errors,
+            'listing' => $updateValues]);
+            exit;
+        } else {
+            $updateFields = [];
+        
+        foreach(array_keys($updateValues) as $field) {
+            $updateFields[] = "{$field} = :{$field}";
+        }
+        $updateFields = implode(', ', $updateFields);
+
+        $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+        $updateValues['id'] = $id;
+        $this->db->query($updateQuery, $updateValues);
+        $_SESSION["success_message"] = 'Listing Updated';
+        redirect('/listings/' . $id);
+        }
     }
 }    
